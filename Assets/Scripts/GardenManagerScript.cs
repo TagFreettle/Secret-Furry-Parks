a00d49da2af0c1804a27e47d976e3ebcbf3449de
Matthew <matthewcenance@gmail.com>
2018-04-21 18:02:54 -0500

    modified:   Assets/New Material.mat.meta 	modified:   Assets/Scripts/GardenManagerScript.cs 	new file:   Assets/Scripts/PlantScript.cs 	new file:   Assets/Scripts/PlantScript.cs.meta 	new file:   Assets/Terrains.meta 	renamed:    Assets/Garden Terrain.asset -> Assets/Terrains/Garden Terrain.asset 	renamed:    Assets/Garden Terrain.asset.meta -> Assets/Terrains/Garden Terrain.asset.meta 	renamed:    Assets/New Terrain.asset -> Assets/Terrains/New Terrain.asset 	renamed:    Assets/New Terrain.asset.meta -> Assets/Terrains/New Terrain.asset.meta 	modified:   Assets/main.unity.meta 	modified:   Assets/Scenes/MainGarden.unity 	modified:   Assets/Scripts/GardenManagerScript.cs 	modified:   Assets/Scripts/PinataScript.cs

--- Assets/Scripts/GardenManagerScript.cs
+++ Assets/Scripts/GardenManagerScript.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class GardenManagerScript : MonoBehaviour {
     //Values
-    public
-    
-	// Use this for initialization
-	void Start () {
+    public int CurrentDay = 0;
+    public int CurrentHour = 0;
+    public int CurrentMinute = 0;
+    public double CurrentSecond = 0;
+
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        // Update the time of the garden.
+        CurrentSecond = CurrentSecond + 5;
+
+        // Check if CurrentSecond is at least 60 and if so then increment the minute by 1
+        if (CurrentSecond >= 60) {
+            CurrentSecond = CurrentSecond - 60;
+            CurrentMinute = CurrentMinute + 1;
+        }
+
+        // Check if CurrentMinute is at least 60 and if so then increment the minute by 1
+        if (CurrentMinute >= 60)
+        {
+            CurrentMinute = CurrentMinute - 60;
+            CurrentHour = CurrentHour + 1;
+        }
+
+        // Check if CurrentHour is at least 24 and if so then increment the minute by 1
+        if (CurrentHour >= 24)
+        {
+            CurrentHour = CurrentHour - 24;
+            CurrentDay = CurrentDay + 1;
+        }
+    }
 }
